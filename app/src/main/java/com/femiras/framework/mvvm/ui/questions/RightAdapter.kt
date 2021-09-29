6cd4f191c631c184b021acc15a290f3a8ac33a0d
evan1234 <s.jahan@nexdecade.com>
2021-09-29 23:10:53 +0600

    anim  done

---
+++ app/src/main/java/com/femiras/framework/mvvm/ui/questions/RightAdapter.kt
@@ -0,0 +1,70 @@
+package com.femiras.framework.mvvm.ui.questions
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.femiras.framework.mvvm.R
+import kotlinx.android.synthetic.main.layout_right.view.*
+
+
+import kotlinx.android.synthetic.main.layout_second.view.*
+import kotlinx.android.synthetic.main.layout_second.view.icon
+
+class RightAdapter (val context: Context) : RecyclerView.Adapter<RightAdapter
+.CustomViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val inflater = LayoutInflater.from(parent?.context)
+        val cellForRow = inflater.inflate(R.layout.layout_right, parent, false)
+        return CustomViewHolder(cellForRow)
+    }
+
+    override fun getItemCount(): Int {
+
+
+        return 15
+    }
+
+    override fun onBindViewHolder(holder:CustomViewHolder, position: Int) {
+
+        Glide.with(context)
+            .load("https://media-eng.dhakatribune.com/uploads/2021/05/photographs-published-in-japan-show-takashi-miyagawa-s-meetings-with-several-different-women-1620026902279.jpg")
+            .into( holder.itemView.profile_image!!)
+//       when (position) {
+//            1 ->
+//                holder.itemView.icon.setImageResource(R.drawable.seconds)
+//
+//            2 ->    holder.itemView.icon.setImageResource(R.drawable.third)
+//            3 ->    holder.itemView.icon.setImageResource(R.drawable.mood_swings)
+//            4 ->    holder.itemView.icon.setImageResource(R.drawable.vitamins)
+//            else ->    holder.itemView.icon.setImageResource(R.drawable.one)
+//        }
+
+
+
+//        Glide.with(context)
+//                .load(product?.get(position)?.Picture)
+//                .into( holder.itemView.img_icon!!)
+//
+//        var prices: String = ""
+//        var quantity: String = ""
+//
+//        holder.itemView.tv_date.text = getStartDate(product?.get(position)?.Created)
+//        holder.itemView.text_name.text = product?.get(position)?.Name
+//        prices = "<b> <font color=#BF3E15>Price ট </font> : </b>" + product?.get(position)?.Price.toString()
+//        quantity = "<b> <font color=#BF3E15>Quantity  </font> : </b>" + product?.get(position)?.Quantity.toString()
+//        holder.itemView.tv_product_price.text = Html.fromHtml(prices!!)
+//        holder.itemView.tv_quantity.text = Html.fromHtml(quantity!!)
+
+
+
+    }
+
+    inner class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+}
--- app/src/main/java/com/femiras/framework/mvvm/ui/questions/RightFragment.kt
+++ app/src/main/java/com/femiras/framework/mvvm/ui/questions/RightFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.femiras.framework.mvvm.R
+import com.femiras.framework.mvvm.ui.home.today.SecondAdapter
+import kotlinx.android.synthetic.main.fragment_right.*
 
 class RightFragment : Fragment() {
-
+    var secondAdapter: RightAdapter?=null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -16,6 +19,16 @@ class RightFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_right, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        secondAdapter = RightAdapter(requireContext())
+        channel_logo_iv?.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false)
+            adapter = secondAdapter
+        }
+
+
+    }
 
 }
